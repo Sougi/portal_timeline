fc72211f2d959236ff3274d59a245e12ba451ada
unknown <soufiane.stdin@gmail.com>
2020-04-11 15:09:33 +0100

    .

--- public/js/app.js
+++ public/js/app.js
@@ -19328,27 +19328,15 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
 /***/ }),
 
-/***/ "./resources/sass/style.scss":
-/*!***********************************!*\
-  !*** ./resources/sass/style.scss ***!
-  \***********************************/
-/*! no static exports found */
-/***/ (function(module, exports) {
-
-// removed by extract-text-webpack-plugin
-
-/***/ }),
-
 /***/ 0:
-/*!*****************************************************************************************!*\
-  !*** multi ./resources/js/app.js ./resources/sass/app.scss ./resources/sass/style.scss ***!
-  \*****************************************************************************************/
+/*!*************************************************************!*\
+  !*** multi ./resources/js/app.js ./resources/sass/app.scss ***!
+  \*************************************************************/
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
 __webpack_require__(/*! C:\WAMP_31_03_2020\www\portal_timeline\resources\js\app.js */"./resources/js/app.js");
-__webpack_require__(/*! C:\WAMP_31_03_2020\www\portal_timeline\resources\sass\app.scss */"./resources/sass/app.scss");
-module.exports = __webpack_require__(/*! C:\WAMP_31_03_2020\www\portal_timeline\resources\sass\style.scss */"./resources/sass/style.scss");
+module.exports = __webpack_require__(/*! C:\WAMP_31_03_2020\www\portal_timeline\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
